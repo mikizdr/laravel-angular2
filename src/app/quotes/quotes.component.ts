ab4fdeabb75d654583e6e6a6b88094d920cbd6c1
Raganor <miroslav.zdravkovic@gmail.com>
2017-05-03 22:07:22 +0200

    Add onDelete() method in quotes.component.ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -32,4 +32,14 @@ export class QuotesComponent implements OnInit {
       );
   }
 
+  onDeleted(quote: Quote)
+  {
+    const position = this.quotes.findIndex(
+      (quoteEl: Quote) => {
+        return quoteEl.id == quote.id;
+      }
+    );
+    this.quotes.splice(position, 1);
+  }
+
 }
