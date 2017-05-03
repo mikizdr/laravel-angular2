7221125e854d0796347bcb16fe8d5c263c72c715
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-05-03 12:27:05 +0200

    Define quote and quotes components and they`s vies

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Quote } from '../quote.interface';
+
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotes: Quote[];
 
   constructor() { }
 
